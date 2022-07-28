9189ea3c7b2efa8824ef5f37724967bc88958af0
3Gigs <yanghuaxuan@gmail.com>
2022-07-28 00:43:37 -0700

    Add firebase

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,26 @@ import App from './App'
 import { Provider } from 'react-redux'
 import { store } from './reduxStore'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDaJ9SH6y7-0z5BCqw1YZOuzmzIgcjxXB0",
+  authDomain: "alanappointment-1898a.firebaseapp.com",
+  projectId: "alanappointment-1898a",
+  storageBucket: "alanappointment-1898a.appspot.com",
+  messagingSenderId: "481710126514",
+  appId: "1:481710126514:web:b8616fd39f1b076dfb20ab",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
     <App />
