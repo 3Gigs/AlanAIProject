ba17a7342c38b2aac857f3c1a1ab3c647ed226a1
3Gigs <yanghuaxuan@gmail.com>
2022-07-31 04:08:48 -0700

    Add sync with database

--- src/Components/calendar.tsx
+++ src/Components/calendar.tsx
@@ -6,11 +6,32 @@ import interactionPlugin from '@fullcalendar/interaction';
 import { RootState } from '../reduxStore';
 import { addEvent } from './calendarSlice';
 import '../App.css';
+import { useEffect } from 'react';
+import { db, firebaseApp } from '../main';
+import { getAuth } from '@firebase/auth';
+import { onValue, ref } from '@firebase/database';
 
 function Calendar()
 {
   const events = useSelector((state: RootState) => state.calendarTest.value)
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    const auth = getAuth(firebaseApp);
+    const email = auth.currentUser?.email;
+    if(!email) {
+      throw new Error("Email not found!");
+    }
+    const query = ref(db, `users/${email.replace(".", "DOT")}`);
+    onValue(query, (snapshot) => {
+      const events = Object.values(snapshot.val().events);
+      console.log(events);
+      for(let event of Object.values(events)) {
+        console.log(event);
+        dispatch(addEvent(event));
+      }
+    });
+  }, []);
 
   function handleClick(arg: any) {
     //dispatch(addEvent());
