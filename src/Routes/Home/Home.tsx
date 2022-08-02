a11c9f4c5111a96135c056599f2984c6c63dc65e
3Gigs <yanghuaxuan@gmail.com>
2022-08-01 22:09:52 -0700

    home page revamp

--- src/Routes/Home/Home.tsx
+++ src/Routes/Home/Home.tsx
@@ -1,11 +1,16 @@
-import { Navigate } from "react-router-dom";
-
-function Home() {
-    return(
-        <div>
-            <h1>Welcome to AlanAppointments!</h1>
+function Home () {
+  return (
+        <div className="Home">
+            <div className="PromoText">
+                <h1 className="PromoText">Alan Appointments</h1>
+                <h1 className="PromoText">Simple scheduling with Alan AI</h1>
+            </div>
+            <div className="PromoArt">
+                <img className="PromoArt_Clock" src="src\assets\promo_clock.svg" />
+                <img className="PromoArt_Calendar" src="src\assets\calendar_home.svg" />
+            </div>
         </div>
-    ) 
+  );
 }
 
 export default Home;
