55641dc87a4e88793a5ff91075491dcc21446b6f
3Gigs <yanghuaxuan@gmail.com>
2022-08-02 02:27:40 -0700

    Change nav bar

--- src/Routes/Navbar/Navi.tsx
+++ src/Routes/Navbar/Navi.tsx
@@ -23,11 +23,10 @@ function Navi () {
         <div>
             <Navbar bg="light" expand="lg">
                 <Container>
-                    <Navbar.Brand href="#home">AlanAppointments</Navbar.Brand>
+                    <Navbar.Brand href="/">AlanAppointments</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="/">Home</Nav.Link>
                             <Nav.Link href="/Dashboard">Dashboard</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
