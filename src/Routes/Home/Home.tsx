b149e8e6d1748dfad46a6b2793f49351bd3684d7
3Gigs <yanghuaxuan@gmail.com>
2022-08-02 00:42:18 -0700

    Homepage rework

--- src/Routes/Home/Home.tsx
+++ src/Routes/Home/Home.tsx
@@ -2,12 +2,13 @@ function Home () {
   return (
         <div className="Home">
             <div className="PromoText">
-                <h1 className="PromoText">Alan Appointments</h1>
-                <h1 className="PromoText">Simple scheduling with Alan AI</h1>
+                <h1 className="PromoText_1">Alan Appointments</h1>
+                <h1 className="PromoText_2">Simple scheduling with Alan AI</h1>
             </div>
             <div className="PromoArt">
                 <img className="PromoArt_Clock" src="/assets/promo_clock.svg" />
                 <img className="PromoArt_Calendar" src="/assets/calendar_home.svg" />
+                <img className="PromoArt_Mic" src="/assets/microphone.svg" />
             </div>
         </div>
   );
